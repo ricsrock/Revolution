aeeef38ae65e2cf377af27b4fc1cfec29e6091e2
ricsrock <ricsrock@gmail.com>
2013-06-27 17:03:09 -0500

    mail settings again again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,12 +66,12 @@ NewrevF::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'localhost',
-    port: 25,
-    domain: 'macserver.local'
-  }
+  config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.smtp_settings = {
+  #   address: 'localhost',
+  #   port: 25,
+  #   domain: 'macserver.local'
+  # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
