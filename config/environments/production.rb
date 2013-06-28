6375d9995da57a8e71da9bc83140abb124765fc1
ricsrock <ricsrock@gmail.com>
2013-06-28 10:18:50 -0500

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,13 +27,13 @@ NewrevF::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.1'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
