f16e9b88b1d2a95e5bfbea9ecf15c51e25df7122
ricsrock <ricsrock@gmail.com>
2013-07-01 09:26:10 -0500

    fix manage contacts view error - rescue nil on person#full_name

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -3,7 +3,7 @@ class DonationsController < ApplicationController
   authorize_resource
   def index
     params[:q] = Donation.fix_params(params[:q]) if params[:q]
-    @search = Donation.page(params[:page]).search(params[:q])
+    @search = Donation.page(params[:page]).includes(:fund, :contribution => [:contributable]).search(params[:q])
     @donations = @search.result
     @range = params[:q][:range_selector_cont] if params[:q]
     @total_set = Donation.search(params[:q]).result
