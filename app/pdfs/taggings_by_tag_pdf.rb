a65b496215e42d306e6b25d01c5e956c25906ad8
ricsrock <ricsrock@gmail.com>
2013-07-08 08:51:30 -0500

    fix taggings by tag summary report

--- app/pdfs/taggings_by_tag_pdf.rb
+++ app/pdfs/taggings_by_tag_pdf.rb
@@ -11,9 +11,9 @@ class TaggingsByTagPdf < Prawn::Document
       columns = %w[Date Person Family Status]
       data = []
       data << columns
-      taggings.sort_by {|t| t.start_date}.each do |tagging|
+      taggings.sort_by {|t| t.start_date.to_date.to_s(:db)}.each do |tagging|
         attrs = []
-        attrs << tagging.start_date
+        attrs << tagging.start_date.to_date.to_s
         attrs << tagging.person.full_name
         attrs << tagging.person.household.family_names
         attrs << tagging.person.attendance_status
@@ -21,7 +21,12 @@ class TaggingsByTagPdf < Prawn::Document
       end
       pdf.table data, row_colors: ["f0f0f0", "fcfcfc"], header: true, cell_style: {borders: []}, width: 540
       pdf.move_down 20
-    end
-    
+    end    
   end
+  
+  def self.detail(pdf, taggings)
+    pdf.move_down 5
+    pdf.text "Sorry, there is no detail layout yet."
+  end
+  
 end
