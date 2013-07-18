4940cfe939099fc25933a4fcf07603e24a6e71db
ricsrock <ricsrock@gmail.com>
2013-07-18 14:24:41 -0500

    removed celebration sentence from guest stickers

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,19 +1,23 @@
 module AttendancesHelper
   
   def celebration_sentence_1(attendance)
-    i = Interjection.all.collect {|a| a.name}.sample + '!'
-    result = i + " We" + "'" + "re so glad to see"
-    result.html_safe
+    unless attendance.person.last_name.downcase.start_with?('guest')
+      i = Interjection.all.collect {|a| a.name}.sample + '!'
+      result = i + " We" + "'" + "re so glad to see"
+      result.html_safe
+    end
   end
   
   def celebration_sentence_2(attendance)
-    person = attendance.person
-    if person.attend_count < 4
-      last_part = "you for the #{person.attend_count.ordinalize} time!"
-    else
-      last_part = "you for the #{person.unique_events_this_year.size.ordinalize} time this year!"
+    unless attendance.person.last_name.downcase.start_with?('guest')
+      person = attendance.person
+      if person.attend_count < 4
+        last_part = "you for the #{person.attend_count.ordinalize} time!"
+      else
+        last_part = "you for the #{person.unique_events_this_year.size.ordinalize} time this year!"
+      end
+      last_part
     end
-    last_part
   end
   
   def week_x_of_y(attendance)
