fd09fba528596f2c32f91f2ff63a55f7ac25bd97
ricsrock <ricsrock@gmail.com>
2013-08-07 15:31:50 -0500

    strip whitespace and add group profile report - 0.1

--- app/pdfs/group_profile_pdf.rb
+++ app/pdfs/group_profile_pdf.rb
@@ -25,8 +25,8 @@ class GroupProfilePdf < Prawn::Document
     y_position = cursor
     bounding_box [0, cursor], :width => 240 do
       text "\u2022 Group Name: #{@group.name}"
-      text "\u2022 Created At: #{@group.created_at.to_time.to_s(:long)}"
-      text "\u2022 Updated At: #{@group.updated_at.to_time.to_s(:long)}"
+      text "\u2022 Created At: #{@group.created_at.to_time.to_s(:long) rescue nil}"
+      text "\u2022 Updated At: #{@group.updated_at.to_time.to_s(:long) rescue nil}"
     end
     bounding_box [250, y_position], :width => 240 do
       text "\u2022 Group Type: #{@group.type}"
