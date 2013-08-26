603dd4e4716fb2a273fe560dca7a28a022143e42
ricsrock <ricsrock@gmail.com>
2013-08-26 12:36:40 -0500

    putting more tasks in background person worker - again

--- app/workers/person_worker.rb
+++ app/workers/person_worker.rb
@@ -1,8 +1,8 @@
 class PersonWorker
   include Sidekiq::Worker
   # @queue = :geocoder_queue
-  def perform(person_id)
-    @person = Person.find(person_id, meeting_id)
+  def perform(person_id, meeting_id)
+    @person = Person.find(person_id)
     @person.set_first_attend
     @person.set_recent_attend
     @person.set_attend_count
