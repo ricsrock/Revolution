2a2b0ab85c238d4336bf49e901502b7e759216e4
ricsrock <ricsrock@gmail.com>
2013-08-28 11:45:11 -0500

    new inquiry layout changes

--- app/helpers/phones_helper.rb
+++ app/helpers/phones_helper.rb
@@ -5,4 +5,8 @@ module PhonesHelper
     result += " " + phone.number[3,3] + "-" + phone.number[6,4]
     result
   end
+  
+  def primary_phone_check_box(p)
+    check_box_tag 'hi', 'there', p.primary?, disabled: true
+  end
 end
